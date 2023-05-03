eebe664c6d8fddfa04795a5a687d44ab7a570e86
moisesPompilio <moisesjtmed@gmail.com>
2023-05-03 14:35:22 -0300

    perf: retirada de loop desnecessario, agora a modificação somente nos items necessarios(inicioCaculo e dip).

--- src/Calculo/CalculoTabela/BeneficioPrincpal/calculoBeneficioPrincipal.js
+++ src/Calculo/CalculoTabela/BeneficioPrincpal/calculoBeneficioPrincipal.js
@@ -17,22 +17,34 @@ async function ajusteDevidoNaDataDibEDipNatabelaPrincipal(informationBeneficioPr
     diasTrabalhadosNoInicioCalculo = validarValorDoDiaSerConsideradoNaTabelaDeCalculo(diasTrabalhadosNoInicioCalculo, mesInicioCalculo);
     diasTrabalhadoNoMesDIP = validarValorDoDiaSerConsideradoNaTabelaDeCalculo(diasTrabalhadoNoMesDIP, mesDip);
 
-    const indexDaDIP = tabelaPrincipal[tabelaPrincipal.length - 1].data.split('/')[0] != '13Salario'? tabelaPrincipal.length - 1 : tabelaPrincipal.length -2;
-
-    await tabelaPrincipal.forEach((item, index) => {
-        if (index == 0 || index == indexDaDIP) {
-            const diasSerConsiderado = index == 0 ? diasTrabalhadosNoInicioCalculo : diasTrabalhadoNoMesDIP;
-            item.data = index == 0 ? inicioCalculo : dip;
-            item.salario = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salario);
-            item.salarioCorrigido = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salarioCorrigido);
-            item.salarioJuros = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salarioJuros);
-            item.salarioTotal = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salarioTotal);
-        }
-    });
+    const indexDaDIP = tabelaPrincipal[tabelaPrincipal.length - 1].data.split('/')[0] != '13Salario' ? tabelaPrincipal.length - 1 : tabelaPrincipal.length - 2;
+    
+    tabelaPrincipal[0] = await retornarLinhaDaTabelaPrincipalComSalarioProporcionalAosDiasTrabalhados(tabelaPrincipal[0], diasTrabalhadosNoInicioCalculo, inicioCalculo)
+    tabelaPrincipal[indexDaDIP] = await retornarLinhaDaTabelaPrincipalComSalarioProporcionalAosDiasTrabalhados(tabelaPrincipal[indexDaDIP], diasTrabalhadoNoMesDIP, dip)
+    
+    // await tabelaPrincipal.forEach((item, index) => {
+    //     if (index == 0 || index == indexDaDIP) {
+    //         const diasSerConsiderado = index == 0 ? diasTrabalhadosNoInicioCalculo : diasTrabalhadoNoMesDIP;
+    //         item.data = index == 0 ? inicioCalculo : dip;
+    //         item.salario = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salario);
+    //         item.salarioCorrigido = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salarioCorrigido);
+    //         item.salarioJuros = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salarioJuros);
+    //         item.salarioTotal = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salarioTotal);
+    //     }
+    // });
 
     return Promise.resolve(tabelaPrincipal);
 }
 
 function calcularValoresDosDiasConsiderados(diasConsiderado, valor) {
     return Math.floor((valor * diasConsiderado / 30) * 100) / 100;
+}
+
+function retornarLinhaDaTabelaPrincipalComSalarioProporcionalAosDiasTrabalhados(item, diasSerConsiderado, data) {
+    item.data = data;
+    item.salario = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salario);
+    item.salarioCorrigido = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salarioCorrigido);
+    item.salarioJuros = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salarioJuros);
+    item.salarioTotal = calcularValoresDosDiasConsiderados(diasSerConsiderado, item.salarioTotal);
+    return item;
 }
