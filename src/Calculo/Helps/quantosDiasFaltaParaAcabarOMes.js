28755000d72e3bda98ece9bc2305a36d30110bd1
moisesPompilio <moisesjtmed@gmail.com>
2023-05-03 14:33:58 -0300

    fix: correção do bug do mes, antes os messes estavam com no maximo 29 dias, agora o mês esta com no maximo 30 dias, que o padrão no calculo previdenciario

--- src/Calculo/Helps/quantosDiasFaltaParaAcabarOMes.js
+++ src/Calculo/Helps/quantosDiasFaltaParaAcabarOMes.js
@@ -2,8 +2,18 @@ export function quantosDiasFaltaParaAcabarOMes(data) {
     const [dia, mes, ano] = data.split('/');
     const date = new Date(ano, mes - 1, dia);
     var ultimoDiaMes = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
-    if(ultimoDiaMes > 30){
-        ultimoDiaMes = 30;
+
+    const diasTrabalhados = ultimoDiaMes - date.getDate() + 1;
+    
+    //dias trabalhados tem que ser no maximo 30 dias 
+    if(diasTrabalhados >= 30){
+        return 30;
     }
-    return ultimoDiaMes - date.getDate();
+    //verificar se é mes de fervereiro fervereiro, se for dias trabalhados for igual ou maior que 28 ele retorna 30
+    else if(parseInt(mes) === 2 && diasTrabalhados >= 28){
+        return 30;
+    }else{
+        return diasTrabalhados;
+    }
+
 }
