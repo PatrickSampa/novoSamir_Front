0a497a580900535c9b89aa93084d0514aa2533e7
PatrickSampa <sampa.mendonca@gmail.com>
2023-07-19 15:29:58 -0300

    atualizando o ip spring

--- src/global.js
+++ src/global.js
@@ -4,14 +4,15 @@ export const userKey = '__gbelpa_user'
 
 // export const samirControle = 'http://10.191.8.199:3003/'
 /* export const samirControle = 'http://10.191.8.242:3010/' */
-export const samirControle = 'http://10.191.8.254:3001/'
+export const samirControle = 'http://10.191.8.198:3001/'
 
 /* export const apiSapiens = 'http://10.191.8.131:80/samir' */
 export const apiSapiens = 'http://10.191.9.2:8080/samir'
 // export const apiSapiens = 'http://172.17.24.236:3002'
 
 /* export const baseApiUrl = 'http://localhost:3000/' */
- export const baseApiUrl = 'http://10.191.8.242:8888'
+ export const baseApiUrl = 'http://10.191.8.254:3004'
+ //http://10.191.8.242:8888
 
 //TESTANDO NOVA BRANCH
 
