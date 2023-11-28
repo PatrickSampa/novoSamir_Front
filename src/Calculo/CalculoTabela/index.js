e899220bed3a1fa1044bf4830590ae5f54335fd2
PatrickSampa <sampa.mendonca@gmail.com>
2023-11-28 11:55:48 -0300

    fix: resolvendo o ero do 13obri, funcao tirada, pois estsava retornando um valor errado

--- src/Calculo/CalculoTabela/index.js
+++ src/Calculo/CalculoTabela/index.js
@@ -3,12 +3,14 @@ import { calculoBeneficioPrincipal } from "./BeneficioPrincpal/calculoBeneficioP
 
 export async function calculoTabelaPrincipal(informationBeneficioPrincpal, beneficiosAcumulados){
   var tabelaPrincipal = await calculoBeneficioPrincipal(informationBeneficioPrincpal);
-  console.log("RETRONO ACUMULADF0ODSSSS " + JSON.stringify(tabelaPrincipal))
+  console.log("RETRONO ACUMULADF0ODSSSS " + JSON.stringify(beneficiosAcumulados))
+  console.log("RETRONO ACUMULADF0ODSSSS " + JSON.stringify(informationBeneficioPrincpal))
   if(beneficiosAcumulados.length > 0){
     const tabelaBeneficioInacumulavel = await calculoBeneficioInacumulavel(informationBeneficioPrincpal, beneficiosAcumulados, tabelaPrincipal);
     //console.log("Acukfasadasda: ",tabelaBeneficioInacumulavel)
     return await tabelaBeneficioInacumulavel;
   }else{
+    console.log("RETONOU PRINCIPAL DIRETO")
     return await tabelaPrincipal
   }
 }
--- src/Calculo/Helps/receberDataParaDecimoTerceiroSalarioRecebido.js
+++ src/Calculo/Helps/receberDataParaDecimoTerceiroSalarioRecebido.js
@@ -1,4 +1,5 @@
 export async function receberRecebidoParaDecimoTerceiroSalario(dataInicial){
+    console.log("FUNCAO DENTRO DO 13 " + dataInicial)
     let mesData = 12 - Number(dataInicial.split("/")[1]);
     console.log(mesData)
     let diaData = 30 - Number(dataInicial.split("/")[0]) >= 15 ? 1 : 0;
